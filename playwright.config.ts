739171db0e12b57a578456961f3cdabfef1d2441
Arunachalam B <techie.arunachalam@gmail.com>
2025-12-29 13:57:55 +0530

    [Arunachalam] Add recording config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    /* Record video of test execution */
+    video: 'on',
+    headless: false,
   },
 
   /* Configure projects for major browsers */
